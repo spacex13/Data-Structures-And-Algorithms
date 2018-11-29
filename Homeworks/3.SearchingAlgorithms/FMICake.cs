98d0a26d0ef1a9c5c911700373c24befdb54aa38
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-11-29 20:53:29 +0200

    added searching alg ex1

---
+++ Homeworks/3.SearchingAlgorithms/FMICake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int days = 0;
+
+            int[] arr = MakeArray(n);
+
+            int m = n;
+
+            while (m > 0)
+            {
+                days++;
+                m /= 2;
+
+                for (int k = 0; k < n; k++)
+                {
+                    if (arr[k] - arr[m] >= 0)
+                        arr[k] -= arr[m];
+                }
+            }
+
+            Console.WriteLine(days);
+        }
+    }
+
+    public static int[] MakeArray(int n)
+    {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = i + 1;
+        }
+
+        return arr;
+    }
+}
