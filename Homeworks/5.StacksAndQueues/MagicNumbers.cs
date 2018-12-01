f028a56fb1327f850949360deb77ebad2f99f1a0
spacex13 <32228357+spacex13@users.noreply.github.com>
2018-12-01 11:57:49 +0200

    added stacks and queues homework

---
+++ Homeworks/5.StacksAndQueues/MagicNumbers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MagicNumbers
+{
+    public static Queue<long> magicNums = new Queue<long>();
+
+    public static void FindMagicNumbers(long n)
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            Queue<long> q = new Queue<long>();
+            q.Enqueue(i);
+
+            while (q.Count > 0)
+            {
+                long currNum = q.Peek();
+                q.Dequeue();
+
+                if (currNum <= n && currNum >= 1)
+                {
+                    magicNums.Enqueue(currNum);
+                }
+
+                if (i == 0 || currNum > n)
+                    continue;
+
+                var lastDigit = currNum % 10;
+
+                var a = lastDigit - 2;
+                var b = lastDigit + 2;
+
+                var first = currNum * 10 + a;
+                var second = currNum * 10 + b;
+
+                if (a >= 0)
+                    q.Enqueue(first);
+                if (b < 10)
+                    q.Enqueue(second);
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+
+        FindMagicNumbers(n);
+
+        var result = magicNums.OrderBy(x => x);
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
---
+++ Homeworks/5.StacksAndQueues/Path.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Stack<int> stack = new Stack<int>();
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            string color = line[0];
+            int k = int.Parse(line[1]);
+            if (color == "green")
+            {
+                int sum = 0;
+                for (int p = 0; p < k; p++)
+                {
+                    if (stack.Count > 0)
+                    {
+                        int data = int.Parse(stack.Peek().ToString());
+                        sum += data;
+                        stack.Pop();
+                    }
+                }
+
+                k = sum;
+            }
+            else if (color == "blue")
+            {
+                int max = 0;
+                for (int p = 0; p < k; p++)
+                {
+                    if (stack.Count > 0)
+                    {
+                        int data = int.Parse(stack.Peek().ToString());
+                        stack.Pop();
+                        if (max < data)
+                            max = data;
+                    }
+                }
+
+                k = max;
+            }
+
+            stack.Push(k);
+        }
+
+        string s = string.Join(" ", stack.Reverse());
+
+        Console.WriteLine(s);
+    }
+}
